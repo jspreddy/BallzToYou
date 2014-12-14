e7faa680f86906c4f47647792be88ad2bf7baebf
Sai Phaninder Reddy Jonnala <jspreddy@yahoo.com>
2014-12-13 22:18:25 -0500

    Object ping and rotate

---
+++ Assets/Scripts/IndependentScripts/ObjectPingAndRotate.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectPingAndRotate : MonoBehaviour {
+	
+	//
+	public float rotationSpeedMultiplier = 50f;
+	public float pingSpeedMultiplier = 5f;
+	public float pingDistanceMultiplier = 0.5f;
+
+	// used in internal calculations.
+	private Vector3 initPosition;
+
+
+	// Use this for initialization
+	void Start () {
+		initPosition = transform.localPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (Vector3.up, rotationSpeedMultiplier * Time.deltaTime);
+		transform.localPosition = new Vector3( initPosition.x, initPosition.y +( pingDistanceMultiplier * Mathf.Sin (Time.timeSinceLevelLoad * pingSpeedMultiplier)), initPosition.z );
+	}
+}
--- Assets/Scripts/PowerUpScript.cs
+++ Assets/Scripts/PowerUpScript.cs
@@ -3,14 +3,12 @@ using System.Collections;
 
 public class PowerUpScript : MonoBehaviour {
 
-	public int duration = 3;
-	public int rotationSpeed = 10;
 
-	// Update is called once per frame
+	void Start(){
+
+	}
+
 	void Update () {
-		//rotate powerup per time.
-		transform.Rotate (Vector3.up, rotationSpeed * Time.deltaTime);
-		// animate position to lerp along y axis.
 
 	}
 
