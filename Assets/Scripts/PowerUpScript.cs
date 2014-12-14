7ef81b1f981cbc92836b0999e5186f93e68d476d
Sai Phaninder Reddy Jonnala <jspreddy@yahoo.com>
2014-12-13 19:30:38 -0500

    base scene and settings

---
+++ Assets/Scripts/PowerUpScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerUpScript : MonoBehaviour {
+
+	public int duration = 3;
+	public int rotationSpeed = 10;
+
+	// Update is called once per frame
+	void Update () {
+		//rotate powerup per time.
+		transform.Rotate (Vector3.up, rotationSpeed * Time.deltaTime);
+		// animate position to lerp along y axis.
+
+	}
+
+	// on colision with player. add powerup to player object powerups list.
+	//destroy game object.
+
+}
