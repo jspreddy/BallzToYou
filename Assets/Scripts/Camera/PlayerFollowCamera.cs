7ad155235f507a7fed0b2f75e5894513233153a1
Sai Phaninder Reddy Jonnala <jspreddy@yahoo.com>
2015-06-14 18:30:50 -0400

    Player movement, Player follow camera,

---
+++ Assets/Scripts/Camera/PlayerFollowCamera.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerFollowCamera : MonoBehaviour {
+
+	private GameObject player;
+	public float maxFollowDistance=6.0f;
+	private float moveDistance = 0.01f;
+	private float catchupTime = 0.5f;
+	// Use this for initialization
+	void Start () {
+		moveDistance = 0.01f;
+		player = GameObject.FindGameObjectWithTag ("Player");
+	}
+
+	void FixedUpdate () {
+		this.transform.LookAt (player.transform, Vector3.up);
+
+
+		float dist = Vector3.Distance (this.transform.position, player.transform.position);
+		float maxMoveDist = dist-this.maxFollowDistance;
+
+		if( dist > maxFollowDistance ){
+			this.moveDistance = maxMoveDist / catchupTime * Time.fixedDeltaTime;
+			this.transform.position = Vector3.MoveTowards(this.transform.position, player.transform.position, this.moveDistance);
+		}
+		else{
+			this.moveDistance = 0.01f;
+		}
+		//Debug.Log ("<color=blue>MoveDist: </color> "+this.moveDistance);
+	}
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float movementSpeed = 20.0f;
+	public float jumpMagnitude = 20.0f;
+	private Rigidbody rb;
+	private Transform cameraTransform;
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+		this.cameraTransform = Camera.main.transform;
+	}
+	
+	void FixedUpdate () {
+		float x = Input.GetAxis ("Horizontal");
+		float z = Input.GetAxis ("Vertical");
+		Vector3 force = new Vector3 (x, 0.0f, z);
+		Vector3 forward;
+		if (cameraTransform.forward.x == 0.0f && cameraTransform.forward.z == 0.0f) {
+			forward = new Vector3(1.0f, 0.0f, 0.0f).normalized;
+			Debug.Log("pointing down.");
+		} else {
+			forward = new Vector3 (cameraTransform.forward.x, 0.0f, cameraTransform.forward.z).normalized;
+		}
+		Debug.DrawLine (this.transform.position, this.transform.position + forward*20);
+
+		Vector3 left = new Vector3(-forward.z, 0.0f, forward.x).normalized;
+		Debug.DrawLine (this.transform.position, this.transform.position + left*20);
+
+		//Debug.DrawLine (this.transform.position, this.transform.position + force *20);
+
+		Vector3 localForce = forward * force.z - left * force.x;
+		Debug.DrawLine (this.transform.position, this.transform.position + localForce *20);
+		//Vector3.
+
+		rb.AddForce (localForce * movementSpeed);
+		if(Input.GetButtonDown("Jump")){
+			rb.AddForce(Vector3.up*jumpMagnitude, ForceMode.Impulse);
+		}
+	}
+}
